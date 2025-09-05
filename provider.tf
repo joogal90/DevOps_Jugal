5d35351fd15d8be5e1f2fed6b37fa10e285feffb
Jugal Verma <joogal89@gmail.com>
2025-09-05 16:10:38 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.41.0"
+    }
+  }
+}
+
+provider "azurerm" {
+features {}
+subscription_id =   "fbac4321-0f41-46f0-baea-f23a62385eb5"
+}
+
+
+
